577619d19e326144e153fbdfcea476e7715c9ffa
dayandavid <33235380+dayandavid@users.noreply.github.com>
2022-03-13 13:01:55 -0400

    Added Django toolbar

--- web/web/settings.py
+++ web/web/settings.py
@@ -25,9 +25,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third Parties
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
