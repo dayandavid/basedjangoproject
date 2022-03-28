85c5cf748a6f606fedf158b9e75d793a90e9ca33
dayandavid <ddavidcoder@gmail.com>
2022-03-28 13:34:06 -0500

    Created TimeStampedModel

---
+++ web/utils/modelhelpers.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class TimeStampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
+
+    class Meta:
+        abstract = True
+
