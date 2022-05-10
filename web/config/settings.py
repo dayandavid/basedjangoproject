baf1ab4bc76ab9f38b9e7a3681525d7659fe1927
dayandavid <33235380+dayandavid@users.noreply.github.com>
2022-05-10 11:30:30 -0400

    Actualizado el settings

--- web/config/settings.py
+++ web/config/settings.py
@@ -32,8 +32,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # Third Parties
-    'ckeditor', 
-    "debug_toolbar",
+    'ckeditor',
+    'debug_toolbar',
+    'django_cleanup.apps.CleanupConfig',
 
     # Local Apps
     'users.apps.UsersConfig',
@@ -72,23 +73,19 @@ TEMPLATES = [
 ]
 
 
-
-
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'web',
+        'NAME': 'db',
         'USER': 'postgres',
         'PASSWORD': '93070223627',
         'HOST': 'localhost',
         'PORT': '5433',
     }
 }
-
 
 
 # Password validation
@@ -115,13 +112,9 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'es'
 
-TIME_ZONE = 'UTC'
-
 USE_I18N = True
 
-USE_TZ = True
-
-SESSION_COOKIE_AGE = 1200
+SESSION_COOKIE_AGE = 60*60  # Sesion de una hora
 
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
@@ -135,15 +128,30 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+# Uploaded media files
+MEDIA_URL = '/uploads/'  # the URL used in our templates for the files
+
+# the absolute file system path to the directory for useruploaded files
+MEDIA_ROOT = BASE_DIR / 'uploads/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Sessions
-
-SESSION_EXPIRE_AT_BROWSER_CLOSE = True
-
 # Users and authentication
 AUTH_USER_MODEL = 'users.CustomUser'
+
+CKEDITOR_CONFIGS = {
+    'default': {
+        'toolbar': 'Custom',
+        'toolbar_Custom': [
+            ['Bold', 'Italic', 'Underline'],
+            ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-',
+                'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'],
+            ['Link', 'Unlink'],
+            ['RemoveFormat', 'Source']
+        ]
+    }
+}
