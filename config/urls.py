b24c10d01e92249199d43d4c314c3f1811c20e2c
dayandavid <33235380+dayandavid@users.noreply.github.com>
2023-03-19 14:03:27 -0400

    Mostrando imagenes en debug y quitando grupos

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,7 @@
 import debug_toolbar
+from django.conf import settings
 from django.contrib import admin
+from django.contrib.auth.models import Group
 from django.urls import path, include
 
 
@@ -12,6 +14,14 @@ urlpatterns = [
     path('__debug__/', include(debug_toolbar.urls)),
 ]
 
+
+# Para que las imagnes se puedan ver en develop
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
 admin.site.site_header  =  "Administración del Sitio"  
 admin.site.site_title  =  "Administración del Sitio"
 admin.site.index_title  =  "Administración"
+admin.site.unregister(Group)
